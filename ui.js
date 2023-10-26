5a657b6b7715c7d04f7ac52ce548fd9a896ca8c7
kidohy15 <kidohy307@gmail.com>
2023-10-27 05:25:23 +0900

    메뉴 클릭 시 해당 페이지의 제목 부분과 내부 컨텐츠 영역이 노출될 수 있도록 처리

--- ui.js
+++ ui.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
+
+  // 메뉴 클릭 시 해당 페이지 영역 확장 효과 이벤트
   $("nav li").click(function () {
+    
+    // class가 on 인 메뉴 한번 더 클릭 시 원래 화면으로 돌아올 수 있게 클래스 제거
+    if ($(this).hasClass("on")) {
+      $(this).removeClass("on");
+      $(".content").removeClass("prev this next");
+      $("#container").css("max-width", "1200px");
+      return;
+    }
+
     $("#container").css("max-width", "100%");
     let id = $(this).attr("data-rol");
     // 기존에 on 클래스가 붙어있다면 다 지워준다
@@ -16,5 +27,13 @@ $(document).ready(function () {
     $("#" + id).addClass("this").nextAll().addClass("next");
 
     console.log("=====", id);
-  })
+  });
+
+  // 로고 클릭 시 다시 메인 페이지로 이동
+  $(".logo_box").click(function () {
+    $("nav li").removeClass("on");
+    $(".content").removeClass("prev this next");
+    $("#container").css("max-width", "1200px");
+  });
+
 })
